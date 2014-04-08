4ae2eb614e2fa3786195376632e135dafc8c1133
Néstor Bethencourt <nestorbs@gmail.com>
2014-04-08 10:27:12 +0100

    red. rejectNegativeNumber

--- TrackEveryPennyAndroid/test/com/nestorbs/DisplayNumberOfTransactionsTest.java
+++ TrackEveryPennyAndroid/test/com/nestorbs/DisplayNumberOfTransactionsTest.java
@@ -1,5 +1,6 @@
 package com.nestorbs;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -17,12 +18,24 @@ public class DisplayNumberOfTransactionsTest {
 	
   @Test
   public void happyPath() throws Exception {
-    BrowseTransactionsActivity browseTransactionsActivity = Robolectric.buildActivity(BrowseTransactionsActivity.class).create().get(); 
-
-    browseTransactionsActivity.displayNumberOfTransactions(13);
-    final TextView transactionsCountView
-        = (TextView) browseTransactionsActivity.findViewById(R.id.transactionsCount);
-
-    assertEquals("13", transactionsCountView.getText().toString());
+	  BrowseTransactionsActivity browseTransactionsActivity = Robolectric.buildActivity(BrowseTransactionsActivity.class).create().get(); 
+	
+	  browseTransactionsActivity.displayNumberOfTransactions(13);
+	  final TextView transactionsCountView
+	    		= (TextView) browseTransactionsActivity.findViewById(R.id.transactionsCount);
+	
+	  assertEquals("13", transactionsCountView.getText().toString());
+  }
+  
+  @Test
+  public void rejectNegativeNumber() throws Exception {
+	  BrowseTransactionsActivity browseTransactionsActivity = 
+			  Robolectric.buildActivity(BrowseTransactionsActivity.class).create().get();
+	  try{
+		  browseTransactionsActivity.displayNumberOfTransactions(-1);
+		  fail("Why did you display a negative number of transations? " + "That's crazy talk!");
+	  } catch (ProgrammerMistake success) {
+		  assertTrue(success.getCause() instanceof IllegalArgumentException);
+	  }
   }
 }
